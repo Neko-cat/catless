af251a08f446c03ab03d77909f51546999a52591
Neko <mathilde.couvreur@gmail.com>
2015-09-18 18:39:43 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Catless</title>
+</head>
+<body>
+	<form action="index.php" method="get">
+  		<input placeholder="Pseudo ou chat..." name="recherche">
+  		<button>Go !</button>
+	</form>
+</body>
+</html>
